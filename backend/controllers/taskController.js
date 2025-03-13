3b40541587e31cf044d3a29e47e8af06505efdb3
Tushar <tusharmudai18@gmail.com>
2025-03-13 11:14:48 +1000

    test added

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,4 +1,13 @@
-
+const Task = require('../models/Task');
+const getTasks = async (
+req,
+res) => {
+try {
+const tasks = await Task.find({ userId: req.user.id });
+res.json(tasks);
+} catch (error) {
+res.status(500).json({ message: error.message });
+}};
 const addTask = async (
 req,
 res) => {
